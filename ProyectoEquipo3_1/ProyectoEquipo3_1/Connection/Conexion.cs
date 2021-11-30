2546485867272099aeb2aea9fa918adb4a98294d
CarloseRochav <78558271+CarloseRochav@users.noreply.github.com>
2021-11-29 20:50:59 -0800

    Admin Model

---
+++ ProyectoEquipo3_1/ProyectoEquipo3_1/Connection/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;//Libreria para poder hacer la conexion con SQL Server
+
+namespace ProyectoEquipo3_1.Connection
+{
+    class Conexion
+    {
+        private static string stringConexion = "server=LAPTOP-E0ORA0CH;database=CitasMedicas2;Trusted_Connection=true";//Linea de conexion a la base de datos
+        private SqlConnection sql_conexion = new SqlConnection(stringConexion);
+
+        public SqlConnection sql_getConnection()//Metodo para obetener conexion
+        {
+            return sql_conexion;
+        }
+    }
+}
--- ProyectoEquipo3_1/ProyectoEquipo3_1/Form1.Designer.cs
+++ ProyectoEquipo3_1/ProyectoEquipo3_1/Form1.Designer.cs
@@ -54,20 +54,23 @@
             // 
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(158, 104);
+            this.button1.Location = new System.Drawing.Point(237, 160);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 51);
+            this.button1.Size = new System.Drawing.Size(260, 78);
             this.button1.TabIndex = 6;
             this.button1.Text = "Registrar Paciente";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(158, 182);
+            this.button2.Location = new System.Drawing.Point(237, 280);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(173, 51);
+            this.button2.Size = new System.Drawing.Size(260, 78);
             this.button2.TabIndex = 7;
             this.button2.Text = "Registrar Medico";
             this.button2.UseVisualStyleBackColor = true;
@@ -76,9 +79,10 @@
             // 
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(158, 261);
+            this.button3.Location = new System.Drawing.Point(237, 402);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(173, 62);
+            this.button3.Size = new System.Drawing.Size(260, 95);
             this.button3.TabIndex = 8;
             this.button3.Text = "Registrar Especialidades";
             this.button3.UseVisualStyleBackColor = true;
@@ -87,9 +91,10 @@
             // 
             this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(158, 341);
+            this.button4.Location = new System.Drawing.Point(237, 525);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(173, 60);
+            this.button4.Size = new System.Drawing.Size(260, 92);
             this.button4.TabIndex = 9;
             this.button4.Text = "Horario Medicos";
             this.button4.UseVisualStyleBackColor = true;
@@ -98,9 +103,10 @@
             // 
             this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(158, 419);
+            this.button5.Location = new System.Drawing.Point(237, 645);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(173, 51);
+            this.button5.Size = new System.Drawing.Size(260, 78);
             this.button5.TabIndex = 10;
             this.button5.Text = "Citas";
             this.button5.UseVisualStyleBackColor = true;
@@ -109,9 +115,10 @@
             // 
             this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox7.Image = global::ProyectoEquipo3_1.Properties.Resources.Regreso;
-            this.pictureBox7.Location = new System.Drawing.Point(417, 12);
+            this.pictureBox7.Location = new System.Drawing.Point(626, 18);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(65, 72);
+            this.pictureBox7.Size = new System.Drawing.Size(98, 111);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox7.TabIndex = 11;
             this.pictureBox7.TabStop = false;
@@ -119,9 +126,10 @@
             // pictureBox6
             // 
             this.pictureBox6.Image = global::ProyectoEquipo3_1.Properties.Resources.Citas;
-            this.pictureBox6.Location = new System.Drawing.Point(71, 407);
+            this.pictureBox6.Location = new System.Drawing.Point(106, 626);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(65, 72);
+            this.pictureBox6.Size = new System.Drawing.Size(98, 111);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox6.TabIndex = 5;
             this.pictureBox6.TabStop = false;
@@ -129,9 +137,10 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = global::ProyectoEquipo3_1.Properties.Resources.Reloj;
-            this.pictureBox5.Location = new System.Drawing.Point(72, 329);
+            this.pictureBox5.Location = new System.Drawing.Point(108, 506);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(65, 72);
+            this.pictureBox5.Size = new System.Drawing.Size(98, 111);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox5.TabIndex = 4;
             this.pictureBox5.TabStop = false;
@@ -139,9 +148,10 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = global::ProyectoEquipo3_1.Properties.Resources.Espe1;
-            this.pictureBox4.Location = new System.Drawing.Point(72, 251);
+            this.pictureBox4.Location = new System.Drawing.Point(108, 386);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(65, 72);
+            this.pictureBox4.Size = new System.Drawing.Size(98, 111);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
@@ -149,9 +159,10 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::ProyectoEquipo3_1.Properties.Resources.Doctor1;
-            this.pictureBox3.Location = new System.Drawing.Point(72, 171);
+            this.pictureBox3.Location = new System.Drawing.Point(108, 263);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(65, 72);
+            this.pictureBox3.Size = new System.Drawing.Size(98, 111);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
@@ -159,9 +170,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::ProyectoEquipo3_1.Properties.Resources.ClienteImagen;
-            this.pictureBox2.Location = new System.Drawing.Point(71, 93);
+            this.pictureBox2.Location = new System.Drawing.Point(106, 143);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(65, 72);
+            this.pictureBox2.Size = new System.Drawing.Size(98, 111);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
@@ -169,18 +181,19 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ProyectoEquipo3_1.Properties.Resources.TituloAdministrador;
-            this.pictureBox1.Location = new System.Drawing.Point(30, 8);
+            this.pictureBox1.Location = new System.Drawing.Point(45, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(381, 78);
+            this.pictureBox1.Size = new System.Drawing.Size(572, 120);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 501);
+            this.ClientSize = new System.Drawing.Size(726, 771);
             this.Controls.Add(this.pictureBox7);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -195,6 +208,7 @@
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sistema De Citas Medicas";
